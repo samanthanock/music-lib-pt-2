4432734ff6e2bfc87d1b27ded6a37565e6fd9d86
Samantha <sammarienock@gmail.com>
2019-02-15 23:40:35 +0000

    creates functions to add tracks and plaliylist as instances

--- music-lib-2.js
+++ music-lib-2.js
@@ -2,7 +2,7 @@
 // ---> Library, Playlist, Track
 
 //
-// Each playlist has an overallRating function which will calculate the rating by averaging the rating of its tracks.
+
 // Each playlist also has a totalDuration function which will summ the duration of all its tracks.
 
 // Library has a NAME and a CREATOR (both strings).
@@ -26,3 +26,25 @@ function Track(title, rating, length) {
   this.rating = rating;
   this.length = length;
 }
+
+// a library has many playlists
+Object.setPrototypeOf(Library.prototype, Playlist.prototype);
+// a playlist has many tracks
+Object.setPrototypeOf(Playlist.prototype, Track.prototype);
+
+// in order to have playlists to add to the libraries, the playlists need tracks
+// in playlists() tracks is an empty array
+// each track needs: title, rating, and length.
+
+//tracks object can be added to an instance of playlist
+Playlist.prototype.addTrack = function(track) {
+  this.tracks.push(track);
+};
+
+//Playlist objects can be added to an instance of a library
+Library.prototype.addPlaylist = function(playlist) {
+  this.playlists.push(playlist);
+};
+
+// Each playlist has an overallRating function which will
+//calculate the rating by averaging the rating of its tracks.
